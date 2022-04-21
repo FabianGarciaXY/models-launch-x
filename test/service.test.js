284a532854bf53fbf5ec04e8e5f0571845439799
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-20 20:39:10 -0500

    Created test file for service module

---
+++ test/service.test.js
@@ -0,0 +1,10 @@
+
+const UserService = require('./../app/user');
+
+describe('Suite of tests for Service class', () => {
+
+    test('Test 1 for Service class', () => {
+
+        expect(2).toBe(3);
+    })
+})
